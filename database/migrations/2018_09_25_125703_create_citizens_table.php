61df0e05d9e6ea17c7976ff560c0a59b4ba014a2
Khan <samir.khan.w@gmail.com>
2018-09-25 16:00:30 +0300

    Added migration for citizens table

---
+++ database/migrations/2018_09_25_125703_create_citizens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitizensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citizens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('reg_id');
+            $table->string('city_ter_id');
+            $table->string('area_ter_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citizens');
+    }
+}
